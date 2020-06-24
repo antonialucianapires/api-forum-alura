4334eaa0cf02c5be51493cef728d8b1b0efa73fe
Antonia Luciana Pires <antonialucianapires@gmail.com>
2020-06-23 22:18:01 -0300

    Commit do projeto inicial + implentação de paginação

---
+++ src/main/java/br/com/alura/forum/config/validacao/ErroValidacaoHandler.java
@@ -0,0 +1,37 @@
+package br.com.alura.forum.config.validacao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErroValidacaoHandler {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public List<ErroDeFormularioDTO> handler(MethodArgumentNotValidException excepetion){
+
+        List<ErroDeFormularioDTO> objDto = new ArrayList<>();
+
+        List<FieldError> fieldErrors = excepetion.getBindingResult().getFieldErrors();
+        fieldErrors.forEach(e -> {
+            String mensagem = messageSource.getMessage(e, LocaleContextHolder.getLocale());
+            ErroDeFormularioDTO error = new ErroDeFormularioDTO(e.getField(), mensagem);
+            objDto.add(error);
+        });
+
+        return objDto;
+    }
+}
