ce04f2f1415795fa916b46b4f87e20bf87a47725
Antonia Luciana Pires <antonialucianapires@gmail.com>
2020-06-23 22:35:42 -0300

    ordenação usando o esquema do pageable do Spring

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -44,10 +46,7 @@ public class TopicosController {
 	
 	@GetMapping
 	public Page<TopicoDto> lista(@RequestParam(required=false) String nomeCurso, 
-							@RequestParam(required=true)int pagina, 
-							@RequestParam(required=true)int quantidade) {
-		
-		Pageable paginacao = PageRequest.of(pagina, quantidade);
+	@PageableDefault(sort="id", direction=Direction.DESC, page=0, size=10)Pageable paginacao) {
 		
 		if(nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
